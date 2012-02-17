92097c1c1260e15c7564998da2ceeded7d2054c0
shurizzle <shura1991@gmail.com>
2012-02-17 16:50:53 +0100

    First commit

---
+++ Scroto/Scroto.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace Scroto
+{
+  partial class Scroto
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.button1 = new System.Windows.Forms.Button();
+      this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+      this.SuspendLayout();
+      // 
+      // button1
+      // 
+      this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.button1.AutoSize = true;
+      this.button1.Location = new System.Drawing.Point(112, 43);
+      this.button1.Name = "button1";
+      this.button1.Size = new System.Drawing.Size(88, 26);
+      this.button1.TabIndex = 0;
+      this.button1.Text = "Shot!";
+      this.button1.UseVisualStyleBackColor = true;
+      this.button1.Click += new System.EventHandler(this.button1_Click);
+      // 
+      // linkLabel1
+      // 
+      this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.linkLabel1.Location = new System.Drawing.Point(12, 9);
+      this.linkLabel1.Name = "linkLabel1";
+      this.linkLabel1.Size = new System.Drawing.Size(288, 28);
+      this.linkLabel1.TabIndex = 1;
+      this.linkLabel1.TabStop = true;
+      this.linkLabel1.Text = "linkLabel1";
+      this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+      this.linkLabel1.Visible = false;
+      this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+      // 
+      // Scroto
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(312, 83);
+      this.Controls.Add(this.linkLabel1);
+      this.Controls.Add(this.button1);
+      this.MaximizeBox = false;
+      this.MaximumSize = new System.Drawing.Size(320, 110);
+      this.MinimumSize = new System.Drawing.Size(320, 110);
+      this.Name = "Scroto";
+      this.Opacity = 0.8D;
+      this.ShowIcon = false;
+      this.Text = "Scroto";
+      this.ResumeLayout(false);
+      this.PerformLayout();
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.Button button1;
+    private System.Windows.Forms.LinkLabel linkLabel1;
+  }
+}
+
---
+++ Scroto/Scroto.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Collections.Specialized;
+using System.Text.RegularExpressions;
+
+namespace Scroto
+{
+  public partial class Scroto : Form
+  {
+    static private string apiKey = "54bf7d24bb8f12f638f40c41abcd65ee";
+    static private Regex ImageRe = new Regex("<original_image>(.*)</original_image>");
+
+    public Scroto()
+    {
+      InitializeComponent();
+    }
+
+    private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+    {
+      System.Diagnostics.Process.Start(e.Link.LinkData.ToString());
+    }
+
+    private void button1_Click(object sender, EventArgs e)
+    {
+      WindowState = FormWindowState.Minimized;
+      linkLabel1.Hide();
+      try
+      {
+        UploadToImgur();
+      }
+      catch (Exception)
+      {
+        WindowState = FormWindowState.Normal;
+        MessageBox.Show("Errore durante l'upload dell'immagine",
+          "Error", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+      }
+    }
+
+    private void UploadToImgur()
+    {
+      NameValueCollection values = new NameValueCollection
+      {
+        {"key", apiKey},
+        {"image", Convert.ToBase64String(TakeScreenshot())}
+      };
+
+      string imgUrl;
+      {
+        byte[] response = new WebClient().UploadValues("http://imgur.com/api/upload.xml", values);
+        string strResponse = System.Text.Encoding.ASCII.GetString(response);
+        imgUrl = ImageRe.Match(strResponse).Groups[1].Value;
+      }
+
+      linkLabel1.Text = imgUrl;
+      linkLabel1.Links.Clear();
+      linkLabel1.Links.Add(0, imgUrl.Length, imgUrl);
+      linkLabel1.Show();
+      Clipboard.SetText(imgUrl);
+      WindowState = FormWindowState.Normal;
+    }
+
+    private byte[] TakeScreenshot()
+    {
+      Size sz = Screen.PrimaryScreen.Bounds.Size;
+      IntPtr hDesk = GetDesktopWindow();
+      IntPtr hSrce = GetWindowDC(hDesk);
+      IntPtr hDest = CreateCompatibleDC(hSrce);
+      IntPtr hBmp = CreateCompatibleBitmap(hSrce, sz.Width, sz.Height);
+      IntPtr hOldBmp = SelectObject(hDest, hBmp);
+      BitBlt(hDest, 0, 0, sz.Width, sz.Height, hSrce, 0, 0, CopyPixelOperation.SourceCopy | CopyPixelOperation.CaptureBlt);
+      Bitmap img = Bitmap.FromHbitmap(hBmp);
+      SelectObject(hDest, hOldBmp);
+      DeleteObject(hBmp);
+      DeleteDC(hDest);
+      ReleaseDC(hDesk, hSrce);
+
+      MemoryStream ms = new MemoryStream();
+      img.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+      img.Dispose();
+
+      return ms.ToArray();
+    }
+
+    [DllImport("gdi32.dll")]
+    static extern bool BitBlt(IntPtr hdcDest, int xDest, int yDest, int
+    wDest, int hDest, IntPtr hdcSource, int xSrc, int ySrc, CopyPixelOperation rop);
+    [DllImport("user32.dll")]
+    static extern bool ReleaseDC(IntPtr hWnd, IntPtr hDc);
+    [DllImport("gdi32.dll")]
+    static extern IntPtr DeleteDC(IntPtr hDc);
+    [DllImport("gdi32.dll")]
+    static extern IntPtr DeleteObject(IntPtr hDc);
+    [DllImport("gdi32.dll")]
+    static extern IntPtr CreateCompatibleBitmap(IntPtr hdc, int nWidth, int nHeight);
+    [DllImport("gdi32.dll")]
+    static extern IntPtr CreateCompatibleDC(IntPtr hdc);
+    [DllImport("gdi32.dll")]
+    static extern IntPtr SelectObject(IntPtr hdc, IntPtr bmp);
+    [DllImport("user32.dll")]
+    public static extern IntPtr GetDesktopWindow();
+    [DllImport("user32.dll")]
+    public static extern IntPtr GetWindowDC(IntPtr ptr);
+  }
+}
