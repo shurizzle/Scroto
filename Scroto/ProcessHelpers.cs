afab6370783905ec616f03af2abdd78ee1faf2c1
shurizzle <shura1991@gmail.com>
2012-02-18 07:20:18 +0100

    Fix single instance traycon bug

---
+++ Scroto/ProcessHelpers.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Scroto
+{
+  public static class ProcessHelpers
+  {
+    class _GetWindow
+    {
+      public IntPtr Result { get; set; }
+      private uint _id = 0;
+
+      public _GetWindow(uint id)
+      {
+        _id = id;
+
+        NativeMethods.EnumWindowsProc ewp = new NativeMethods.EnumWindowsProc(Check);
+        NativeMethods.EnumWindows(ewp, 0);
+      }
+
+      private bool Check(int hWnd, int lParam)
+      {
+        uint pid = 0;
+        NativeMethods.GetWindowThreadProcessId(new IntPtr(hWnd), out pid);
+
+        if (pid == _id)
+        {
+          Result = new IntPtr(hWnd);
+          return false;
+        }
+
+        return true;
+      }
+    }
+
+    static public IntPtr GetWindowHandle(this System.Diagnostics.Process proc)
+    {
+      return (new _GetWindow((uint)proc.Id)).Result;
+    }
+
+    static public bool IsVisible(this System.Diagnostics.Process proc)
+    {
+      NativeMethods.WINDOWINFO pwi = new NativeMethods.WINDOWINFO();
+      IntPtr handle = proc.MainWindowHandle;
+      return NativeMethods.GetWindowInfo(handle, ref pwi);
+    }
+
+    static public void Show(this System.Diagnostics.Process proc)
+    {
+      if (!proc.IsVisible())
+        NativeMethods.ShowWindow(proc.GetWindowHandle(), NativeMethods.SW_SHOW);
+
+      NativeMethods.PostMessage(
+        proc.MainWindowHandle,
+        NativeMethods.WM_SHOWME,
+        IntPtr.Zero,
+        IntPtr.Zero);
+
+      NativeMethods.SetForegroundWindow(proc.MainWindowHandle);
+    }
+  }
+
+  public static class Process
+  {
+    static public System.Diagnostics.Process Instance
+    {
+      get
+      {
+        var current = System.Diagnostics.Process.GetCurrentProcess();
+        foreach (var process in System.Diagnostics.Process.GetProcessesByName(current.ProcessName))
+        {
+          if (process.Id != current.Id)
+          {
+            current = process;
+            break;
+          }
+        }
+
+        return current;
+      }
+    }
+  }
+}
--- Scroto/Program.cs
+++ Scroto/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using Application = System.Windows.Forms.Application;
 using Mutex = System.Threading.Mutex;
-using Process = System.Diagnostics.Process;
 
 namespace Scroto
 {
@@ -14,30 +13,18 @@ namespace Scroto
     static void Main()
     {
       bool createdNew = true;
-      new Mutex(true, "{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}", out createdNew);
+      var m = new Mutex(true, "{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}", out createdNew);
+
       if (createdNew)
       {
         Application.EnableVisualStyles();
         Application.SetCompatibleTextRenderingDefault(false);
         Application.Run(new Scroto());
+        GC.KeepAlive(m);
       }
       else
       {
-        var current = Process.GetCurrentProcess();
-        foreach (var process in Process.GetProcessesByName(current.ProcessName))
-        {
-          if (process.Id != current.Id)
-          {
-            NativeMethods.PostMessage(
-              process.MainWindowHandle,
-              NativeMethods.WM_SHOWME,
-              IntPtr.Zero,
-              IntPtr.Zero);
-
-            NativeMethods.SetForegroundWindow(process.MainWindowHandle);
-            break;
-          }
-        }
+        Process.Instance.Show();
       }
     }
   }
