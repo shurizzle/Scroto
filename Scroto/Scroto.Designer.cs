3bc23965234464ae800a19fb8a0d2f7b83b2c880
shura <shura1991@gmail.com>
2013-03-29 00:36:07 +0100

    Scroto now can shoot single windows

--- Scroto/Scroto.Designer.cs
+++ Scroto/Scroto.Designer.cs
@@ -34,8 +34,10 @@
       this.linkLabel1 = new System.Windows.Forms.LinkLabel();
       this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
       this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
-      this.esciToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
       this.shootToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.esciToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+      this.button2 = new System.Windows.Forms.Button();
+      this.windowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
       this.contextMenuStrip1.SuspendLayout();
       this.SuspendLayout();
       // 
@@ -45,7 +47,7 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
       this.button1.AutoSize = true;
-      this.button1.Location = new System.Drawing.Point(112, 43);
+      this.button1.Location = new System.Drawing.Point(65, 40);
       this.button1.Name = "button1";
       this.button1.Size = new System.Drawing.Size(88, 26);
       this.button1.TabIndex = 0;
@@ -73,7 +75,7 @@
       this.notifyIcon1.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
       this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
       this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-      this.notifyIcon1.Text = "notifyIcon1";
+      this.notifyIcon1.Text = "Scroto";
       this.notifyIcon1.Visible = true;
       this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
       // 
@@ -81,29 +83,48 @@
       // 
       this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.shootToolStripMenuItem,
+            this.windowToolStripMenuItem,
             this.esciToolStripMenuItem});
       this.contextMenuStrip1.Name = "contextMenuStrip1";
-      this.contextMenuStrip1.Size = new System.Drawing.Size(107, 48);
-      // 
-      // esciToolStripMenuItem
-      // 
-      this.esciToolStripMenuItem.Name = "esciToolStripMenuItem";
-      this.esciToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
-      this.esciToolStripMenuItem.Text = "Esci";
-      this.esciToolStripMenuItem.Click += new System.EventHandler(this.esciToolStripMenuItem_Click);
+      this.contextMenuStrip1.Size = new System.Drawing.Size(153, 92);
       // 
       // shootToolStripMenuItem
       // 
       this.shootToolStripMenuItem.Name = "shootToolStripMenuItem";
-      this.shootToolStripMenuItem.Size = new System.Drawing.Size(106, 22);
+      this.shootToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
       this.shootToolStripMenuItem.Text = "Shoot!";
       this.shootToolStripMenuItem.Click += new System.EventHandler(this.shootToolStripMenuItem_Click);
+      // 
+      // esciToolStripMenuItem
+      // 
+      this.esciToolStripMenuItem.Name = "esciToolStripMenuItem";
+      this.esciToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+      this.esciToolStripMenuItem.Text = "Esci";
+      this.esciToolStripMenuItem.Click += new System.EventHandler(this.esciToolStripMenuItem_Click);
+      // 
+      // button2
+      // 
+      this.button2.Location = new System.Drawing.Point(159, 40);
+      this.button2.Name = "button2";
+      this.button2.Size = new System.Drawing.Size(88, 26);
+      this.button2.TabIndex = 2;
+      this.button2.Text = "Window";
+      this.button2.UseVisualStyleBackColor = true;
+      this.button2.Click += new System.EventHandler(this.button2_Click);
+      // 
+      // windowToolStripMenuItem
+      // 
+      this.windowToolStripMenuItem.Name = "windowToolStripMenuItem";
+      this.windowToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+      this.windowToolStripMenuItem.Text = "Window";
+      this.windowToolStripMenuItem.Click += new System.EventHandler(this.windowToolStripMenuItem_Click);
       // 
       // Scroto
       // 
       this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
       this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-      this.ClientSize = new System.Drawing.Size(312, 83);
+      this.ClientSize = new System.Drawing.Size(304, 71);
+      this.Controls.Add(this.button2);
       this.Controls.Add(this.linkLabel1);
       this.Controls.Add(this.button1);
       this.MaximizeBox = false;
@@ -127,6 +148,8 @@
     private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
     private System.Windows.Forms.ToolStripMenuItem esciToolStripMenuItem;
     private System.Windows.Forms.ToolStripMenuItem shootToolStripMenuItem;
+    private System.Windows.Forms.Button button2;
+    private System.Windows.Forms.ToolStripMenuItem windowToolStripMenuItem;
   }
 }
 
